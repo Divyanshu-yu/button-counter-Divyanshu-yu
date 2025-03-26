5fa7da139095e5c8cca47adc64b35440294718c3
Divyanshu-yu <divyanshuy46@gmail.com>
2025-03-26 19:47:06 +0530

    Button Counter done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+
+       const [Count,setCount]=useState(0);
+       const handleClick =()=>
+       {
+        setCount(Count + 1);
+       }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+     <p>Button clicked {Count} times </p>
+     <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
 
-export default App
+/*
+   git add .
+   git commit -m "added"
+   git push
+
+  ctrl-J for closing the terminal opening also
+*/
+export default App ;
